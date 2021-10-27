a239ad3053ba7fc6bccbfbc78308af025974b075
Guddu Kumar <coldmilkhottea@gmail.com>
2021-10-27 10:52:32 +0530

    custom scrollbar created using react

---
+++ ReactJS/Scrollbar/kontactguddu/scrollbar/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import './MyComponents/styleScrollbar.css';
+import Scrollbar from './MyComponents/Scrollbar';
+
+function App() {
+  return (
+    <>
+    <Scrollbar />
+    </>
+  );
+}
+
+export default App;
